f1a2a7ccbf7367451e8848de8918d603715cfc42
Tom <tom.maxwell@cambridgeaudio.com>
2018-01-26 10:26:30 +0000

    Adding a reusable function to replace v. similar setting options

--- src/tate/options.js
+++ src/tate/options.js
@@ -12,12 +12,36 @@ type OptionsType = {
 
 } 
 
+export const defaultOptions = () => {
+  return {
+    layout: '3:2',
+    ratio: 'default',
+    thing: ''
+  }
+}
+
 // keep this upto date so it can be tested
 export const defaultLayout = {layout: '3:2'};
 export const defaultRatio = {ratio: 'default'};
 
 const Options = (options: OptionsType = {}) => {
-  return hasLayout(hasRatio(options));
+  options = {
+    layout: '10:9',
+    ratio: 'complex'
+  }
+  return mapOptions(options, defaultOptions());
+  // return hasLayout(hasRatio(options));
+}
+
+const mapOptions = (options: OptionsType = {}, defaults) => {
+  return Object.keys(defaults).reduce((acc, value) => {
+    if (!acc[value]) {
+      return Object.assign({}, {
+        [value]: defaults[value]
+      }, acc);
+    }
+    return acc;
+  }, options);
 }
 
 const hasRatio = (options: OptionsType) => {
